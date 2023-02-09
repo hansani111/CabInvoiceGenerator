ded34354cf11cc9e50a528c1344d96623553c553
hansani111 <hansanideshmukh894@gmail.com>
2023-02-09 09:03:36 +0530

    added: multiple ride

--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -20,4 +20,11 @@ public class CabInvoiceTest {
         double actualFare = cabInvoice.calculateFare(0.1, 0.5);
         Assertions.assertEquals(5, actualFare);
     }
+
+    @Test
+    void givenNumberOfRidesShouldReturnTotalFare(){
+        Ride[] arr = {new Ride(2.0,5.0),new Ride(3.0,7.0),new Ride(0.1,0.3)};
+        double fare1 = cabInvoice.calculateFare(arr);
+        Assertions.assertEquals(67,fare1);
+    }
 }
